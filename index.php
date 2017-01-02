d0f6b41904e555e99afd1b82ca1b7f1c0567e41c
Philipp Franck <philipp.franck@dainst.de>
2017-01-02 14:11:04 +0100

    V 0.1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file index.php
+ *
+ *
+ * @package ojsDainstZenonlink
+ *
+ * @brief Wrapper for ojsDainstZenonlink plugin.
+ *
+ */
+
+require_once('ojs-dainst-zenonlink.inc.php');
+return new ojsDainstZenonlink();
+
+?>
