d27f3e755122c78d05c0e8b2176dcca6bf3d3dc9
Simon Hohl <simon.hohl@dainst.org>
2022-08-30 20:18:06 +0200

    Remove hacky webserver with direct access to database

--- api/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-$debugmode 		    = false;
-$errorReporting 	= true;
-$allowedIps		    = array();
-$allowedSets 		= array('ANGULAR_POST', 'POST', 'GET', 'URL');
-$serverclass 		= 'zenonApi';
-$settings = array();
-$plainOutput        = false;
-?>
